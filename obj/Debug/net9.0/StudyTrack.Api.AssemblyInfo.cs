5188d68c515666faebf3602b0b81b88d8fc95d13
AndyMio17 <andymiomejia17@gmail.com>
2025-07-12 20:38:57 -0500

    Uploading first progress in the development branch

--- obj/Debug/net9.0/StudyTrack.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/StudyTrack.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudyTrack.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c6760e6deea2a74b81ad82f257c653daceb620")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudyTrack.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudyTrack.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
